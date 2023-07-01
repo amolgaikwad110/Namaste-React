d8ef0972f79590afb291498925581cf76ed37c77
Amol Gaikwad <amolgaikwad110@gmail.com>
2023-07-01 19:05:39 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,86 @@
+/**
+ * <div id="parent">
+ *      <div id="child1">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h2 tag</h2>
+ *      </div>
+ * </div>
+ * 
+ * ReactElement(Object) ==>> HTML (Browser Understands)
+ * 
+ */
+
+const parent = React.createElement(
+    "div",
+    {
+        id: "parent",
+    },
+    [
+        React.createElement(
+            "div",
+            {
+                id: "child1",
+            },
+            [
+                React.createElement(
+                    "h1",
+                    {
+                        id: "heading11",
+                    },
+                    "I am h1 tag"
+                ),
+                React.createElement(
+                    "h2",
+                    {
+                        id: "heading12",
+                    },
+                    "I am h2 tag"
+                ),
+            ]
+        ),
+        React.createElement(
+            "div",
+            {
+                id: "child2",
+            },
+            [
+                React.createElement(
+                    "h1",
+                    {
+                        id: "heading21",
+                    },
+                    "I am h1 tag"
+                ),
+                React.createElement(
+                    "h2",
+                    {
+                        id: "heading22",
+                    },
+                    "I am h2 tag"
+                ),
+            ]
+        ),
+    ]
+);
+console.log(parent);
+
+// Creating Element using React
+const heading = React.createElement(
+    "h1",
+    {
+        id: "heading",
+        className: "heading",
+    },
+    "Hello World in React"
+);
+console.log(heading);
+
+// Creating root in React using ReactDOM
+// Everything will load / render inside root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Namaste React
+
+# Episode 1
+
+1. Hello World in HTML
+2. Hello World in JS
+3. Hello World in React
+4. Adding React CDN
+5. Creating ReactElement
